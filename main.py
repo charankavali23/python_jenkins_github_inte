657ba0e4af57a0ae386fc7c4f561887760a819d4
Charan Kavali <charankavali2309@gmail.com>
2024-07-30 20:18:55 +0530

    changed to api code

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("CHARAN: Hello World!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Flask!"
+
+if __name__ == '__main__':
+    app.run(debug=False)
